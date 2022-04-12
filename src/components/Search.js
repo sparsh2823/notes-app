fb3a7d3426f0958f0159637226a436e05f927901
Sparsh Singh Rathore <sparsh2823@gmail.com>
2022-04-12 09:46:50 +0530

    Update Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,7 +6,7 @@ const Search=({handleSearchNote})=>{
         <div className='search'>
         <MdSearch className="search-icon" size='1.3em'/>
         <input type='text'
-         onChange = {(event)=>handleSearchNote(event.target.value)} 
+         onChange = {(event)=>handleSearchNote(event.target.value.toLowerCase())} 
          placeholder='type to search....'/>
         </div>
     )
